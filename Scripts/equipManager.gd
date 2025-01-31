1373cb028ef0bd82be6301cb2709bd770badcc58
Divas Subedi <33047984+dsubedi753@users.noreply.github.com>
2025-01-31 02:21:56 -0600

    ImpalaHorn

--- Scripts/equipManager.gd
+++ Scripts/equipManager.gd
@@ -3,22 +3,22 @@ extends Control
 var player:Player
 var ibexHorn:IbexHorn = null
 var deerHorn:DeerHorn = null
-var ramHorn:RamHorn = null
+var imapalaHorn:ImpalaHorn = null
 var bullHorn:BullHorn = null
-var elkHorn:BullHorn = null
-var buffaloHorn:BullHorn = null
+var elkHorn:ElkHorn = null
+var buffaloHorn:BuffaloHorn = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().process_frame 
 	player = get_tree().get_first_node_in_group("player")
-	ibexHorn = get_node("/root/World1/Player/IbexHorn")
-	deerHorn = get_node("/root/World1/Player/DeerHorn")
-	ramHorn = get_node("/root/World1/Player/RamHorn")
-	elkHorn = get_node("/root/World1/Player/ElkHorn")
-	bullHorn = get_node("/root/World1/Player/BullHorn")
-	buffaloHorn = get_node("/root/World1/Player/BuffaloHorn")
+	ibexHorn = get_node("/root/World1/EquipManager/IbexHorn")
+	deerHorn = get_node("/root/World1/EquipManager/DeerHorn")
+	imapalaHorn = get_node("/root/World1/EquipManager/ImpalaHorn")
+	elkHorn = get_node("/root/World1/EquipManager/ElkHorn")
+	bullHorn = get_node("/root/World1/EquipManager/BullHorn")
+	buffaloHorn = get_node("/root/World1/EquipManager/BuffaloHorn")
 	
 	player.unequip_horn.connect(_on_unequip_horn)
 	player.equip_horn.connect(_on_equip_horn)
